64ce0bf68722b4f18e3b2c2c1e7773b20673f8a3
john2453317 <john2453317@gmail.com>
2026-09-17 07:52:36 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,5 @@
 # Assign your variables below, each on its own line!
-
 caesat = "Graham"
 praline = "John"
 viking = "Teresa"
-
 # Put your variables above this line, and uncomment the following 3 line of codes.
