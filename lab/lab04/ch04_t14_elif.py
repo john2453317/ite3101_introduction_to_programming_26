e8c528b65f6867e7b22f7acdc74d1aae3dc0112f
john2453317 <john2453317@gmail.com>
2026-09-24 08:11:04 +0000

    [skip actions] lab/lab04/ch04_t14_elif.py

--- lab/lab04/ch04_t14_elif.py
+++ lab/lab04/ch04_t14_elif.py
@@ -1,7 +1,7 @@
 def greater_less_equal_5(answer: int) -> int:
-    if answer >=5 :
+    if int >=5 :
         return 1
-    elif answer <=5 :
+    elif int <=5 :
         return -1
     else:
         return 0
