1c0644a0531e1356255237c2c2eb98deb12ec657
john2453317 <john2453317@gmail.com>
2026-09-24 07:57:19 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,7 +7,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = (2 == 2) and "ABC" == "ABC"
 
 # Make me false!
-bool_three = None
+bool_three = 
 
 # Make me true!
 bool_four = None
