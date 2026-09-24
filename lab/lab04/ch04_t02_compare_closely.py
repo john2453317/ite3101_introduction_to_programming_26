a57b55fa8440fe690823dcc9d3b8bb17e64340eb
john2453317 <john2453317@gmail.com>
2026-09-24 07:29:38 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -13,4 +13,4 @@ bool_three = True
 bool_four = False
 >>> 99 != (98 + 1) True >>> 99 == (98 + 1) False
 # Set this to True if 99 != (98 + 1) or to False otherwise.
-bool_five = flase
+bool_five = False
