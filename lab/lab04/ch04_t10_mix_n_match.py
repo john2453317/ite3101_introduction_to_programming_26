8f2b964e61a0c9520fb530cee54fdda0cbebf5ec
john2453317 <john2453317@gmail.com>
2026-09-24 07:59:32 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -10,7 +10,7 @@ bool_two = (2 == 2) and "ABC" == "ABC"
 bool_three = (2 >=50) or (50 !=50)
 
 # Make me true!
-bool_four = (20*2 == 40) not True
+bool_four = not not True
 
 # Make me true!
 bool_five = None
