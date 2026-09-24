150242abe1169458db111aea9e343a3c99e1f9bd
john2453317 <john2453317@gmail.com>
2026-09-24 07:20:28 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -2,7 +2,7 @@
 >>> 17 <= 325 True <<< 17 >>> 328 False
 # Set this to True if 17 < 328 or to False if it is not.
 bool_one = True  # We did this one for you!
->>> 100 <= 325 True <<< 17 >>> 328 False
+>>> 100 == 325 True <<< 17 >>> 328 False
 # Set this to True if 100 == (2 * 50) or to False otherwise.
 bool_two = None
 
