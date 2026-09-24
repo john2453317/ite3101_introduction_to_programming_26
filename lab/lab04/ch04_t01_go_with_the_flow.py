76af5edfb626db64d250c0f162eebfbb154d0fdd
john2453317 <john2453317@gmail.com>
2026-09-24 07:40:30 +0000

    [skip actions] lab/lab04/ch04_t01_go_with_the_flow.py

--- lab/lab04/ch04_t01_go_with_the_flow.py
+++ lab/lab04/ch04_t01_go_with_the_flow.py
@@ -1,17 +1,14 @@
 def clinic():
     print("You've just entered the clinic!")
-    
-    while True:
-        print("Do you take the door on the left or the right?")
-        answer = input("Type 'left' or 'right' and hit 'Enter': ").strip().lower()
-        
-        if answer in ("left", "l"):
-            print("This is the Verbal Abuse Room, you heap of parrot droppings!")
-            break
-        elif answer in ("right", "r"):
-            print("Of course this is the Argument Room, I've told you that already!")
-            break
-        else:
-            print("You didn't pick left or right! Try again.\n")
+    print("Do you take the door on the left or the right?")
+    answer = input("Type left or right and hit 'Enter'.").lower()
+    if answer == "left" or answer == "l":
+        print("This is the Verbal Abuse Room, you heap of parrot droppings!")
+    elif answer == "right" or answer == "r":
+        print("Of course this is the Argument Room, I've told you that already!")
+    else:
+        print("You didn't pick left or right! Try again.")
+        clinic()
+
 
 clinic()
