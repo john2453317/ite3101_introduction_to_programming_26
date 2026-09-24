7dedb023a4a6d46a48d2ee4687aaeb7cfd7c5b6e
john2453317 <john2453317@gmail.com>
2026-09-24 07:16:41 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -1,5 +1,5 @@
 # Assign True or False as appropriate on the lines below!
->>> 17 == 17 True <<< 328 == 5 False
+>>> 17 == 17 True <<< 328 == 328 False
 # Set this to True if 17 < 328 or to False if it is not.
 bool_one = True  # We did this one for you!
 
